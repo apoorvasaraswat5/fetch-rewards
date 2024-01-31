bad5f37df2be2a981d4dd8591bd89dbc86a6f957
Apoorva Saraswat <100483287+apoorvasaraswat5@users.noreply.github.com>
2024-01-30 20:40:52 -0500

    Added comments in the code

--- fetch.py
+++ fetch.py
@@ -3,11 +3,16 @@ from utility import calculate_points, generate_unique_id
 
 app = Flask(__name__)
 
-
+# storing receipts point data in memory
 points_storage = {}
 
 @app.route('/receipts/process', methods=['POST'])
 def process_receipts():
+    """
+    This endpoint processes receipts and calculates
+    points for the receipt based on some business rules
+    :return: unique id for the receipt
+    """
     receipt_data = request.json
 
     points = calculate_points(receipt_data)
@@ -20,9 +25,15 @@ def process_receipts():
 
 @app.route('/receipts/<receipt_id>/points', methods=['GET'])
 def get_points_for_receipt(receipt_id):
+    """
+    This endpoint returns the points based on
+    receipt's unique id, and throws error if
+    the id does not exist
+    :param receipt_id: unique id of receipt
+    :return: points for the receipt id
+    """
 
     if receipt_id not in points_storage:
-        # return jsonify({'description': "Invalid receipt id!"})
         return Response("Invalid receipt Id!", status=404)
 
     points = points_storage[receipt_id]
--- utility.py
+++ utility.py
@@ -1,13 +1,23 @@
 import uuid
 from math import ceil
 
+
 def generate_unique_id():
-    # unique ID for the receipt
-    uuid_key =  str(uuid.uuid4())
+    """
+    Generates unique ID for the receipt
+    :return: unique ID
+    """
+    uuid_key = str(uuid.uuid4())
     return uuid_key
 
 
 def calculate_points(data):
+    """
+    Calculates points against the receipt data based
+    on some business rules
+    :param data: receipt data
+    :return: points
+    """
     points = 0
 
     retailer_name = data.get("retailer","")
@@ -24,6 +34,9 @@ def calculate_points(data):
 
 
 def get_points_on_retailer_name(retailer_name):
+    """
+    One point for every alphanumeric character in the retailer name
+    """
     points = 0
     for c in retailer_name:
         if c.isalnum():
@@ -32,6 +45,10 @@ def get_points_on_retailer_name(retailer_name):
 
 
 def get_points_on_total(total):
+    """
+    50 points if the total is a round dollar amount with no cents.
+    25 points if the total is a multiple of 0.25.
+    """
     points = 0
     if float(total).is_integer():
         points += 50
@@ -42,6 +59,12 @@ def get_points_on_total(total):
 
 
 def get_points_on_items(items):
+    """
+    5 points for every two items on the receipt.
+    If the trimmed length of the item description is a multiple of 3,
+    multiply the price by 0.2 and round up to the nearest integer.
+    The result is the number of points earned.
+    """
     points = 0
     points += (len(items)//2)*5
 
@@ -54,6 +77,10 @@ def get_points_on_items(items):
 
 
 def get_points_on_date_and_time(purchase_date,purchase_time):
+    """
+    6 points if the day in the purchase date is odd.
+    10 points if the time of purchase is after 2:00pm and before 4:00pm.
+    """
     points = 0
     date_tokens = purchase_date.split("-")
     day = int(date_tokens[2])
